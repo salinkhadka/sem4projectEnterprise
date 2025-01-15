bb4cc120f0a9994aaa6d5791b4326b3726c23377
Ritesh230087 <riteshdaas01@gmail.com>
2025-01-16 00:00:29 +0545

    drawer update

--- lib/components/drawer.dart
+++ lib/components/drawer.dart
@@ -5,3 +5,14 @@ class MyDrawer extends StatefulWidget {
   @override
   _MyDrawerState createState() => _MyDrawerState();
 }
+class _MyDrawerState extends State<MyDrawer> {
+  TextStyle _style = TextStyle(
+    fontSize: 15,
+  );
+  late final PreferenceProvider preferenceProvider;
+  final ValueNotifier<bool> expanded = ValueNotifier<bool>(false);
+  @override
+  void initState() {
+    super.initState();
+    preferenceProvider = Provider.of<PreferenceProvider>(context, listen: false);
+  }
