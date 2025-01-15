06c63c4f1e7cef93fa5c64f5851976384545e1e5
Ritesh230087 <riteshdaas01@gmail.com>
2025-01-15 23:22:22 +0545

    drawer

---
+++ lib/components/drawer.dart
@@ -0,0 +1,7 @@
+class MyDrawer extends StatefulWidget {
+  final HomePageState? homePageState;
+
+  const MyDrawer({Key? key, this.homePageState}) : super(key: key);
+  @override
+  _MyDrawerState createState() => _MyDrawerState();
+}
