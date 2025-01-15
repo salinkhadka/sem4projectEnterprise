cd708c4d9de6b35175de498e995b1f178da9b8cd
Ritesh230087 <riteshdaas01@gmail.com>
2025-01-16 00:07:49 +0545

    drawer code  update

--- lib/components/drawer.dart
+++ lib/components/drawer.dart
@@ -16,3 +16,56 @@ class _MyDrawerState extends State<MyDrawer> {
     super.initState();
     preferenceProvider = Provider.of<PreferenceProvider>(context, listen: false);
   }
+
+   @override
+    Widget build(BuildContext context) {
+      return Container(
+        color: const Color(0xffffffff),
+        width: MediaQuery.of(context).size.width * 0.8,
+        padding: const EdgeInsets.only(
+          left: 25,
+        ),
+        child: Padding(
+          padding: MediaQuery.of(context).viewPadding,
+          child: ListView(
+            physics: BouncingScrollPhysics(),
+            children: <Widget>[
+              // SizedBox(
+              //   height: 15,
+              // ),
+              Align(
+                  alignment: Alignment.centerLeft,
+                  child: Image.asset(
+                    "assets/images/logo.png",
+                    height: 100,
+                    width: 250,
+                    fit: BoxFit.cover,
+                  )),
+              // SizedBox(
+              //   height: 15,
+              // ),
+              // SizedBox(
+              //   height: 10,
+              // ),
+              drawerWidget(
+                  iconString: dashboardIcon,
+                  title: 'Home',
+                  onTap: () {
+                    Navigator.of(context).pop();
+                  }),
+              drawerWidget(
+                iconString: accountsIcon,
+                title: 'Cash & Bank Balance',
+                onTap: () => Navigator.pushNamed(
+                  context,
+                  Routes.account,
+                ),
+              ),
+              drawerWidget(
+                iconString: categoriesIcon,
+                title: 'Categories',
+                onTap: () => Navigator.pushNamed(
+                  context,
+                  Routes.category,
+                ),
+              ),
