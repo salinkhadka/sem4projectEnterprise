3bed672f71a1b98fd17a3e84ed98d1589bfdc2be
Ritesh230087 <riteshdaas01@gmail.com>
2025-01-17 23:16:45 +0545

    account page

--- lib/screens/account_page.dart
+++ lib/screens/account_page.dart
@@ -71,3 +71,189 @@ class _AccountPageState extends State<AccountPage> {
       },
     );
   }
+
+  Widget _buildBody() {
+      return SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+          child: StreamBuilder<List<AccountDataModel>>(
+              stream: AppDatabase().myDatabase.accountDao.watchAccountInformation(),
+              builder: (context, snapshot) {
+                final List<AccountDataModel> _accounts = snapshot.data ?? [];
+                final double totalBalance = _accounts.fold<double>(0.0, (value, data) {
+                  return value = value + data.incomeExpenseAmount + data.balance;
+                });
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    SizedBox(height: 20.0),
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(30.0),
+                          bottomRight: Radius.circular(30.0),
+                          bottomLeft: Radius.circular(30.0),
+                        ),
+                        color: const Color(0xff7635c7),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.only(top: 20, left: 20),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            AdaptiveText(
+                              TextModel('Current Balance'),
+                              style: TextStyle(
+                                fontSize: 20,
+                                color: const Color(0xffffffff),
+                                fontWeight: FontWeight.w600,
+                              ),
+                              textAlign: TextAlign.left,
+                            ),
+                            SizedBox(height: 20.0),
+                            Row(
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: <Widget>[
+                                Text(
+                                  _formatBalanceWithComma(totalBalance),
+                                  style: TextStyle(
+                                    fontSize: 31,
+                                    color: const Color(0xffffffff),
+                                    fontWeight: FontWeight.w700,
+                                  ),
+                                ),
+                                SizedBox(
+                                  width: 3,
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(bottom: 4.0),
+                                  child: AdaptiveText(
+                                    TextModel('NPR'),
+                                    style: TextStyle(
+                                      fontSize: 14,
+                                      color: const Color(0xffb182ec),
+                                      fontWeight: FontWeight.w700,
+                                    ),
+                                    textAlign: TextAlign.left,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            SizedBox(height: 20.0),
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    if (snapshot.hasData)
+                      ListView.builder(
+                        shrinkWrap: true,
+                        physics: NeverScrollableScrollPhysics(),
+                        itemCount: _accounts.length,
+                        itemBuilder: (context, _index) {
+                          return Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 10),
+                            child: Container(
+                              width: double.maxFinite,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(30.0),
+                                  bottomRight: Radius.circular(30.0),
+                                  bottomLeft: Radius.circular(30.0),
+                                ),
+                                color: const Color(0xffffffff),
+                              ),
+                              child: ListTile(
+                                leading: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Icon(
+                                    VectorIcons.fromName(_accounts[_index].iconName ?? 'hornbill', provider: IconProvider.FontAwesome5),
+                                    color: Configuration().accountIconColor,
+                                    size: 25,
+                                  ),
+                                  // child:
+                                ),
+                                title: AdaptiveText(
+                                  TextModel(_accounts[_index].name, nepaliName: _accounts[_index].nepaliName),
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: const Color(0xff272b37),
+                                    height: 1.4285714285714286,
+                                  ),
+                                  textAlign: TextAlign.left,
+                                ),
+                                subtitle: FutureBuilder(
+                                    future: AppDatabase().myDatabase.accountTypeDao.getById(_accounts[_index].accountTypeId),
+                                    builder: ((context, snapshot) => snapshot.hasData
+                                        ? AdaptiveText(
+                                            TextModel(snapshot.data!.name, nepaliName: snapshot.data!.nepaliName),
+                                            style: TextStyle(
+                                              fontSize: 12,
+                                              color: Colors.grey,
+                                            ),
+                                            textAlign: TextAlign.left,
+                                          )
+                                        : SizedBox.shrink())),
+                                trailing: Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: <Widget>[
+                                    Text(
+                                      _formatBalanceWithComma(_accounts[_index].balance + _accounts[_index].incomeExpenseAmount),
+                                      style: TextStyle(
+                                        fontSize: 18,
+                                        color: const Color(0xff1e1e1e),
+                                        fontWeight: FontWeight.w700,
+                                      ),
+                                      textAlign: TextAlign.center,
+                                    ),
+                                    PopupMenuButton<int>(
+                                      icon: Icon(
+                                        Icons.more_vert,
+                                        color: Colors.grey,
+                                      ),
+                                      onSelected: (value) async {
+                                        if (value == 1) {
+                                          _deleteDialog(_accounts[_index]);
+                                        } else {
+                                          await showDialog(
+                                            context: context,
+                                            builder: (context) => ChangeNotifierProvider<PreferenceProvider>(
+                                              create: ((context) => PreferenceProvider()),
+                                              child: AccountDialog(accountDataModel: _accounts[_index]),
+                                            ),
+                                          );
+                                        }
+                                      },
+                                      itemBuilder: (context) => [
+                                        PopupMenuItem(value: 2, child: AdaptiveText(TextModel("Edit"))),
+                                        if (!_accounts[_index].isSystem)
+                                          PopupMenuItem(
+                                            value: 1,
+                                            child: AdaptiveText(
+                                              TextModel('Delete'),
+                                              style: TextStyle(color: Colors.black),
+                                            ),
+                                          ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          );
+                        },
+                      )
+                    else
+                      Center(
+                        child: CircularProgressIndicator(),
+                      )
+                  ],
+                );
+              }),
+        ),
+      );
+    }
