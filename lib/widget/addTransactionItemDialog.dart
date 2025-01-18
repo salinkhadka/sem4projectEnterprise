d326cb5af74dcf455e426864398198da75d4d2ef
Ritesh230087 <riteshdaas01@gmail.com>
2025-01-18 23:42:44 +0545

    add transaction

--- lib/widget/addTransactionItemDialog.dart
+++ lib/widget/addTransactionItemDialog.dart
@@ -1,6 +1,24 @@
+import 'package:byaparlekha/components/adaptive_text.dart';
+import 'package:byaparlekha/components/customDropDown.dart';
+import 'package:byaparlekha/components/extra_componenets.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
 class AddTransactionItemDialog extends StatefulWidget {
   final Function(TransactionItemData) onItemAdd;
   const AddTransactionItemDialog({super.key, required this.onItemAdd});
   @override
   _AddTransactionItemDialogState createState() => _AddTransactionItemDialogState();
 }
+
+class _AddTransactionItemDialogState extends State<AddTransactionItemDialog> {
+  final TextEditingController quantityController = TextEditingController();
+  final TextEditingController amountController = TextEditingController();
+  int? itemId;
+  var _formKey = GlobalKey<FormState>();
+  late final Lang language;
+  @override
+  void initState() {
+    language = Provider.of<PreferenceProvider>(context, listen: false).language;
+    super.initState();
+  }
